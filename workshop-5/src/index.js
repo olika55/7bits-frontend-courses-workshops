da39f80be5e3124648ca95736dac24563b4ad861
olika55 <olika-omsk@yandex.ru>
2019-11-10 18:56:51 +0600

    node-sass version corrected; array with fetched and jsoned promises go into Promise.all

--- workshop-5/src/index.js
+++ workshop-5/src/index.js
@@ -17,40 +17,46 @@ document.addEventListener("DOMContentLoaded", function() {
   const content = $('.content');
 
   let arrProm = [];
-  let arr = [];
 
   content.append(spinnerTemplate());
 
   urls.map((url) => {
-      // console.log(url);
-     arrProm.push(fetch(url));
+      // console.log("url: ", url);
+      fetch(url)
+          .then((response) => {
+              if(response.ok) {
+                  // let prom = response.json().catch(e => {console.log("incorrect json ", response.url, ". ", e)});
+                  let prom = response.json();
+                  if(prom) {
+                      arrProm.push(prom);
+                  }
+                  console.log("prom: ");
+                  console.log(prom);
+              }
+              else {
+                  let p = new Promise((resolve,reject) => {console.log("rejected!!");reject(new Error(response.url+", status: "+response.status));});
+                  // p.catch(err=>{console.log(err);});
+                  arrProm.push(p);
+
+                  console.log("p: ");
+                  console.log(p);
+                  // throw new Error(item.url+", status: "+item.status);
+              }
+          });
+     // arrProm.push(fetch(url));
   });
+
+    console.log("arrProm:");
+    console.log(arrProm);
 
     Promise.all(arrProm)
         .then(values => {
-            // console.log(values);
-            values.map((item) => {
-                // console.log(item);
-                if(item.ok) {
-                        let it = item.json().catch(e => {console.log("incorrect json ", item.url, ". ", e)});
-                        arr.push(it);
-                }
-                else {
-                    let p = new Promise((resolve,reject) => {console.log("rejected!!");reject(new Error(item.url+", status: "+item.status));});
-                    p.catch(err=>{console.log(err);});
-                    // arr.push(p);
-                    // throw new Error(item.url+", status: "+item.status);
-                }
-            });
-            // console.log("arr:");
-            // console.log(arr);
-            return arr;
-        })
-       .then(arr => {
-            arr.map((item) => {
-                // console.log('parsed json', item);
+            console.log("values:");
+            console.log(values);
+            /*values.map((item) => {
+                console.log('parsed json or an error promise: ', item);
                 item.then((res) => {
-                    // console.log("res: ", res);
+                    console.log("res: ", res);
                     if(res) {
                         setTimeout(() => {}, 5000);
                         res.data.forEach((it) => {
@@ -59,7 +65,7 @@ document.addEventListener("DOMContentLoaded", function() {
                     }
 
                 })
-            })
+            })*/
         })
         .finally(() => { setTimeout(()=>$("#loading").remove(), 5000)})
         .catch(ex => {
